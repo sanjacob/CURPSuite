9e8ee0c766bc411c20cd632d94db864b2d0494a4
jacobszpz <jacobszpz@protonmail.com>
2021-08-20 22:57:43 -0500

    Add mypy and tox to dev deps. Update .gitignore for vim. Fix package metadata.

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 from setuptools import setup
-setup()
+from curp.__about__ import (__title__, __version__, __summary__, __uri__, __author__, __email__, __license__)
+
+setup(
+    name=__title__,
+    version=__version__,
+    description=__summary__,
+    url=__uri__,
+    author=__author__,
+    author_email=__email__,
+    license=__license__)
